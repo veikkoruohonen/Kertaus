4316564736a45824b2673b59c959fcc0d2194a44
veikkoruohonen <veikkoruohonen47@gmail.com>
2026-03-04 22:54:57 +0200

    Delete PycharmProjects directory

--- PycharmProjects/PythonProject/Tuumat senttimetreiksi kunnes negatiivinen.py
+++
@@ -1,6 +0,0 @@
-while True:
-    tuumat = float(input("Anna tuumamäärä (negatiivinen lopettaa): "))
-    if tuumat < 0:
-        break
-    sentit = tuumat * 2.54
-    print(f"{tuumat} tuumaa = {sentit:.2f} cm")
--- PycharmProjects/PythonProject1/Keitto.py
+++
@@ -1,9 +0,0 @@
-nimi = input("Kerro nimesi: ")
-
-if nimi == "Matti":
-    print("Seuraava, kiitos!")
-else:
-    annokset = int(input("Montako keittoannosta? "))
-    hinta = annokset * 5.90
-    print(f"Kokonaishinta on {hinta}")
-    print("Seuraava, kiitos!")
